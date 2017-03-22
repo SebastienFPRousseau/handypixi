7a42426dd91557a35794d463ec0a816d2d6e1e42
SebastienFPRousseau <sebastienfp.rousseau@gmail.com>
2017-03-22 17:59:13 +0100

    [WIP] Add object2D's class Object2D part 2.

--- src/displayObject/object2D/Object2D.js
+++ src/displayObject/object2D/Object2D.js
@@ -11,16 +11,28 @@
 */
 
 const { Container } = require("./../container/Container.js");
+const { BitmapText } = require("./../container/BitmapText.js");
+const { Mask } = require("./../container/mask/Mask.js");
+const { Shape } = require("./../container/mask/shape/Shape.js");
+const { Sprite } = require("./../container/mask/sprite/Sprite.js");
+const { AnimatedSprite } = require("./../container/mask/sprite/AnimatedSprite.js");
+const { TilingSprite } = require("./../container/mask/sprite/TilingSprite.js");
+const { SimpleText } = require("./../container/mask/sprite/SimpleText.js");
+const { Mesh } = require("./../container/mesh/Mesh.js");
+const { Rope } = require("./../container/mesh/Rope.js");
+const { Plane } = require("./../container/mesh/Plane.js");
+const { NineSlicePlane } = require("./../container/mesh/NineSlicePlane.js");
 
 class Object2D
 {
 	/**
 	 * constructor
 	 * This function is used in order to build a Object2D.
+	 * @param {Container}  container  The container for this object.
 	 */
-	constructor()
+	constructor(container = new Container())
 	{
-		this._out = new Container();
+		this._out = container;
 		this._looks = [];
 		this._mask = null;
 		this._parent = null;
@@ -66,6 +78,17 @@ class Object2D
 	 * This function is a setter for the member _parent.
 	 * @param {Object2D} parent The parent of this object. 
 	 */
+	set parent(parent)
+	{
+		if (!(parent instanceof Object2D))
+			throw new TypeError("parent must be a Object2D.");
+
+		if (this._parent !== null)
+			this._parent.removeChild(this);
+
+		this._parent = parent;
+		this._out.setParent(parent.out);
+	}
 
 	/**
 	 * looks
@@ -91,82 +114,188 @@ class Object2D
 
 	/**
 	 * addLook
-	 * This function is used in order to
+	 * This function is used in order to add and apply a Look on this object.
+	 * @param {Look}  look  The look to apply on this object.
 	 */
 	
 	/**
 	 * addLooks
-	 * This function is used in order to
+	 * This function is used in order to add and apply some Looks on this object.
+	 * @param {Look[]}  looks  The looks to apply on this object.
 	 */
 	
 	/**
 	 * getLookAt
-	 * This function is used in order to
+	 * This function is used in order to get a Look at the given indice.
+	 * @param {Number}  id  The indice to get the look.
+	 * @return {Look} The corresponding look.
 	 */
 	
 	/**
 	 * removeLookAt
-	 * This function is used in order to
+	 * This function is used in order to remove a Look at the given indice.
+	 * @param {Number}  id  The indice of the look to remove.
 	 */
 	
 	/**
 	 * removeLooks
-	 * This function is used in order to
+	 * This function is used in order to remove all looks of this object.
 	 */
 	
 	/**
 	 * addChild
-	 * This function is used in order to
+	 * This function is used in order to add a child for this object.
+	 * @param {Object2D}  obj  The object to add as a child.
 	 */
-	
+	addChild(obj)
+	{
+		if (!(obj instanceof Object2D))
+			throw new TypeError("obj must be a Object2D.");
+
+		obj.parent = this;
+		this.children.push(obj);
+	}
+
 	/**
 	 * addChildren
-	 * This function is used in order to
+	 * This function is used in order to add some children for this object.
+	 * @param {Object2D[]} objs The objects to add as children.
 	 */
-	
+	addChildren(objs)
+	{
+		if (!Array.isArray(objs))
+		{
+			throw new TypeError("objs must be an array.");
+		}
+		else 
+		{
+			for (let i = 0, l = objs.length; i < l; i++)
+			{
+				if (!(objs[i] instanceof Object2D))
+					throw new TypeError("Can't add the "+ i +" element, it must be an Object2D");
+
+				objs[i].parent = this;
+				this._children.push(objs[i]);
+			}
+		}
+	}
+
 	/**
 	 * getChildAt
-	 * This function is used in order to
+	 * This function is used in order to get the child at the given indice.
+	 * @param {Number}  id  The indice to get the child.
+	 * @return {Object2D} The corresponding child.
 	 */
-	
+	getChildAt(id)
+	{
+		if ({}.toString.call(id) !== "[object Number]")
+			throw new TypeError("id must be a number.");
+
+		return this._children[id];
+	}
+
 	/**
 	 * removeChildAt
-	 * This function is used in order to
+	 * This function is used in order to remove a child at the given indice.
+	 * @param {Number}  id  The indice of the child to remove.
 	 */
-	
+	removeChildAt(id)
+	{
+		if ({}.toString.call(id) !== "[object Number]")
+			throw new TypeError("id must be a number.");
+
+		if (id < this._children.length && id >= 0)
+		{
+			let child = this._children[i];
+			child._parent = null;
+			this._children.splice(i, 1);
+			this._out.removeChild(child.out);
+		}
+	}
+
+	/**
+	 * removeChild
+	 * This function is used in order to remove a child of this object.
+	 * @param {Object2D}  obj  The child to remove from this object.
+	 */
+	removeChild(obj)
+	{
+		if (!(obj instanceof Object2D))
+			throw new TypeError("obj must be a Object2D.");
+
+		for (let i = 0, l = this._children.length; i < l; i++)
+		{
+			if (obj === this._children[i])
+				this.removeChildAt(i);
+		}
+	}
+
 	/**
 	 * removeChildren
-	 * This function is used in order to
+	 * This function is used in order to remove all children of this object.
+	 * @param {Number}  start  The beginning indice included.
+	 * @param {Number}  end  The ending indice excluded.
 	 */
-	
+	removeChildren(start = 0, end = this._children.length)
+	{
+		if ({}.toString.call(start) !== "[object Number]")
+			throw new TypeError("start must be a number.");
+
+		if ({}.toString.call(end) !== "[object Number]")
+			throw new TypeError("end must be a number.");
+
+		if (start >= 0  && end <= this._children.length)
+		{
+			for (i = start; i < end; i++)
+				this.removeChildAt(i);
+		}
+	}
+
 	/**
 	 * clone
-	 * This function is used in order to
+	 * This function is used in order to clone this object.
+	 * @return {Object2D} A copy of this object.
 	 */
 
 	/**
 	 * destroy
-	 * This function is used in order to
+	 * This function is used in order to destroy this object.
+	 * @param {Object}  options  Options for the destruction.
 	 */
 	
 	/**
 	 * update
-	 * This function is used in order to
+	 * This function is used in order to update this object.
 	 */
 	
 	/**
 	 * applyMask
-	 * This function is used in order to
+	 * This function is used in order to apply a mask on this object.
+	 * @param {Mask}  mask  The mask to apply.
 	 */
-	
+	applyMask(mask)
+	{
+		if (!(mask instanceof Mask))
+			throw new TypeError("mask must be a Mask.");
+
+		this._mask = mask;
+		this._out.mask = mask.out;
+	}
+
 	/**
 	 * removeMask
-	 * This function is used in order to
+	 * This function is used in order to remove the applied mask of this object.
 	 */
-	
+	removeMask()
+	{
+		this._mask = null;
+		this._out.mask = null;
+	}
+
 	/**
 	 * getSprite
-	 * This function is used in order to
+	 * This function is used in order to get the resulting Sprite of this object to use it as a mask. 
+	 * @return {Sprite} The resulting Sprite from this object.
 	 */
 }
 
